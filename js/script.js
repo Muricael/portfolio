7240e0e4e6712d43269bfa25620c179e29781520
Muricael <alexandre.datascience@gmail.com>
2024-03-24 18:26:00 +0100

    Ajout page tableau

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function() {
-    header.classList.toggle ("sticky", window.scrollY >120);
-} );
+    header.classList.toggle ("sticky", window.scrollY > 10);
+});
 
-let menu = document.querySelector("#menu-icon");
+let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
+
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
